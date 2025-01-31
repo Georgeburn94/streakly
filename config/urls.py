79db7f4b26dd3c644fbacb91902365ad7d0e4270
Amanda Flynn <amandaflynn98@gmail.com>
2025-01-31 13:50:29 +0000

    Quick change to config urls order,deleted comment in base

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('streakly.urls')),
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
-    path('', include('streakly.urls')),
+    
 ]
